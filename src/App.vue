191b9b2c8bcf425ef77a88d51cd6a999566530de
Hippolyte Larzul <hippolarzul@gmail.com>
2024-08-14 23:10:36 +0200

    add bubbles

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Navbar />
     <router-view />
+    <FooterView />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Navbar from './components/NavBar.vue'
+import FooterView from './components/Footer.vue'
 
 export default defineComponent({
   components: {
-    Navbar
+    Navbar,
+    FooterView
   }
 })
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer>
+    <div class="footer-content">
+      <span>&copy; 2024 Mon Portfolio. Tous droits réservés.</span>
+      <a href="#" class="legal-link">Mentions légales</a>
+      <a href="#" class="legal-link">Politique de confidentialité</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+  position: static;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  line-height: 60px;
+  background-color: #d8beff;
+  color: #fff;
+  text-align: center;
+  font-size: 14px;
+  margin-bottom: 0;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
+.legal-link {
+  color: #d8beff;
+  text-decoration: none;
+}
+
+.legal-link:hover {
+  text-decoration: underline;
+}
+</style>
