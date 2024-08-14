7b20284b7f1142632f456deb083cd67cc59919ed
Hippolyte Larzul <hippolarzul@gmail.com>
2024-08-14 15:39:06 +0200

    add pages for about

---
+++ src/components/about/ClimbingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="climbing-page">
+    <h1>About Climbing</h1>
+    <!-- Add your content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClimbingPage'
+  // Add your component logic here
+}
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
