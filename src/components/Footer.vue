49c1c65859c1ef2a228f32a0222f24eac36ca53a
Hippolyte Larzul <hippolarzul@gmail.com>
2024-08-19 19:29:39 +0200

    update dev mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,27 @@
 <template>
   <footer>
-    <div class="footer-content">
-      <span class="mx-4">&copy; 2024 Portfolio de Hippolyte Larzul. Tous droits réservés.</span>
-      <a href="#" class="legal-link">Mentions légales</a>
-      <!-- <a href="#" class="legal-link mx-2">Politique de confidentialité</a> -->
+    <div class="footer-container">
+      <button class="back-button pt-1" @click="goBack">
+        <i class="fas fa-arrow-left"></i>
+      </button>
+      <div class="footer-content">
+        <span class="mx-4">&copy; 2024 Portfolio de Hippolyte Larzul. Tous droits réservés.</span>
+      </div>
     </div>
   </footer>
 </template>
 
 <script>
-export default {
-  name: 'Footer'
-}
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Footer',
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
+  }
+})
 </script>
 
 <style scoped>
@@ -19,25 +29,56 @@ footer {
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 40px;
-  line-height: 40px;
   background-color: #bedcff;
   color: #000000;
-  text-align: center;
   font-size: 14px;
   margin-bottom: 0;
+  height: 35px;
+  padding-top: 7px;
+}
+
+.footer-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+}
+
+.back-button {
+  background-color: transparent;
+  border: none;
+  color: #000000;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: color 0.3s ease;
+  display: flex;
+  align-items: center;
+}
+
+.back-button i {
+  margin-right: 8px;
+}
+
+.back-button:hover {
+  color: #ffffff;
+}
+
+.back-button:focus {
+  outline: none;
 }
 
 .footer-content {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-grow: 1;
   text-align: center;
 }
 
 .legal-link {
   color: #000000;
   text-decoration: none;
+  margin-left: 10px;
 }
 
 .legal-link:hover {
